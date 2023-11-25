7978e6aafbe826896305630095e346b1a1bf58ab
Kaung Myat <kaungmyatkyaw255@example.com>
2023-11-25 21:28:10 +0630

    added product update annd delete with image (File::delete())

--- ecommerce-back/app/Http/Controllers/ProductController.php
+++ ecommerce-back/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 
 class ProductController extends Controller
@@ -59,7 +60,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Category $category)
+    public function show(Product  $products)
     {
         //
     }
@@ -67,33 +68,50 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Category $category)
+    public function edit(Product $product)
     {
-        return view('categories.edit', compact('category'));
+        $categories = Category::all();
+        $product = Product::where("id", $product->id)->with("category")->first();
+        return view('products.edit', compact('product', 'categories'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(Request $request, Product $product)
     {
         $request->validate([
-            "name" => "required"
-        ], [
-            "name.required" => "Category name is required."
+            "category_id" => "required",
+            "name" => "required",
+            "price" => "required",
+            "description" => "required",
         ]);
-        Category::where("id", $category->id)->update([
-            "name" => $request->name,
+        // if user choose file
+        if ($request->file("image")) {
+            File::delete(public_path("/images/" . $product->image));
+            $file = $request->file("image");
+            $file_name = uniqid() . $file->getClientOriginalName();
+            $file->move(public_path("/images"), $file_name);
+        } else {
+            $file_name = $product->image;
+        }
+        Product::where("id", $product->id)->update([
+            'name' => $request->name,
+            'price' => $request->price,
+            'description' => $request->description,
+            'image' => $file_name,
+            'category_id' => $request->category_id,
         ]);
-        return redirect()->back()->with("success", "Category updated success.");
+        return redirect()->route('products.index')->with("success", "Product updated success");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category)
+    public function destroy(Product $product)
     {
-        Category::destroy($category->id);
-        return redirect()->back()->with("success", "Category deleted success.");
+        Product::where("id", $product->id)->delete();
+        File::delete(public_path("/images/" . $product->image));
+        return redirect()->route('products.index')->with("success", "Product deleted success");
     }
 }
--- ecommerce-back/resources/views/products/edit.blade.php
+++ ecommerce-back/resources/views/products/edit.blade.php
@@ -2,23 +2,56 @@
 
 @section('content')
     <div class="container mx-auto p-4">
-        <a href="{{ route('categories.index') }}"
+        <a href="{{ route('products.index') }}"
             class="text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">
             Back
         </a>
     </div>
     <div class="container mx-auto p-4">
-        <form action="{{ route('categories.update', $category->id) }}" method="POST">
+        <form action="{{ route('products.update', $product->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Enter Category name</label>
-                <input type="text" id="name" name="name" value="{{ $category->name }}"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-                    placeholder="category name...">
+                <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select your
+                    Category</label>
+                <select id="category" name="category_id"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    @foreach ($categories as $c)
+                        <option value="{{ $c->id }}" @if ($c->id === $product->category_id) selected @endif>
+                            {{ $c->name }}</option>
+                    @endforeach
+                </select>
             </div>
+            <div class="mb-6">
+                <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Enter Product name</label>
+                <input type="text" id="name" name="name" value={{ $product->name }}
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="Product name..." required>
+            </div>
+            <div class="mb-6">
+                <label for="price" class="block mb-2 text-sm font-medium text-gray-900 ">Enter Product price</label>
+                <input type="number" id="price" name="price" value={{ $product->price }}
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                    placeholder="Product Price..." required>
+            </div>
+            <div class="mb-6">
+                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Upload
+                    file</label>
+                <input value={{ $product->file }}
+                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    aria-describedby="user_avatar_help" id="user_avatar" name="image" type="file">
+                <img src="{{ asset('/images/' . $product->image) }}" class=" w-40 mt-3" alt="">
+            </div>
+            <div class="mb-6">
+                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter
+                    Description</label>
+                <textarea id="message" rows="4" name="description"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write your thoughts here...">{{ $product->description }}</textarea>
+            </div>
+
             <button type="submit"
-                class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">Submit</button>
+                class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center ">update</button>
         </form>
     </div>
 @endsection
